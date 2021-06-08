4118ff5f72d628e776af6c8d8a2ddf24250b21c3
SaintJul <favor.julius@gmail.com>
2021-06-08 04:13:19 -0400

    More notes for websites utility

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-console.log("Money_Penny")
 fetch("https://raw.githubusercontent.com/SaintJul/json-server-collection/main/bond-movie-stats/db.json")
 .then(function(res){
     return res.json()
@@ -9,11 +8,13 @@ fetch("https://raw.githubusercontent.com/SaintJul/json-server-collection/main/bo
 })
 
 
-// Display the names of the bonds films
-
-// Allows the user to pick their favorite. possibly add a like or heart ~ will think about this after program is running
-    //Once one is clicked the user could choose another subset: car(~maybe add on a picture of the car ), location, film-length, writer
-// ~get other movies with the same writer or actor playing bond
+// Display the names of the bonds films +
 
 
-//Display the info then get css to style the page and info
+//structurally ~ 
+    //I'll have a h1 say 007
+    //print the names of the movies in an h3 or h4 (horizontal list??)
+    // once the user clicks a movie they have an option to like it and get additional info ~
+    // once the category for additional infomation is clicked, the user could also look at movies with the same actor.
+
+//Display the info then get css to style the page and info.
