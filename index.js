ba7a82c9a27b6c012fe74ca1f21bc51a2f917f6a
SaintJul <favor.julius@gmail.com>
2021-06-08 03:07:30 -0400

    Goals for the project

---
+++ index.js
@@ -0,0 +1,4 @@
+// Display the names of the bonds films
+// Allows the user to pick their favorite. possibly add a like or heart ~ will think about this after program is running
+    //Once one is clicked the user could choose another subset: car(~maybe add on a picture of the car ), location, film-length, writer
+// ~get other movies with the same writer or actor playing bond
